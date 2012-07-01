60cc104da0acc37c710341a6f4fb52861b613a2f
Coco <corentin.chataignon@gmail.com>
2012-07-01 11:50:21 +0200

    commit3

---
+++ Pendu/Pendu1/Pendu.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pendu
+{
+	class Pendu
+	{
+		public string mot;
+		private List<string> mots = new List<string> { "cochon", "vache", "taureau", "poule", "mouton", "chien" };
+		public List<char> motca = new List<char>();
+
+		public Pendu()
+		{
+			Random ran = new Random();
+			int index = ran.Next(0, mots.Count);
+			mot = mots[index];
+			foreach (char l in mot)
+			{
+				motca.Add('*');
+			}
+		}
+		public string jeu(char lettre)
+		{
+			foreach (var iter in mot.Select((Value, Index) => new { Value, Index }))
+			{
+				if (iter.Value == lettre)
+				{
+					motca[iter.Index] = lettre;
+				}
+			}
+			return string.Join("", motca.ToArray());
+		}
+
+	}
+}
---
+++ Pendu/Pendu1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pendu
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Pendu pen = new Pendu();
+			int coup = 11;
+			bool ok = false;
+			Console.WriteLine(string.Join("", pen.motca.ToArray()));
+ 			while (!ok)
+			{
+				try
+				{
+					Console.Write("Lettre: ");
+					string let = Console.ReadLine();
+					Console.WriteLine(pen.jeu(let[0]));
+
+					if (string.Join("", pen.motca.ToArray()) == pen.mot)
+					{
+						Console.WriteLine("Bravo !");
+						Console.Write("Appuyez sur une touche pour continuer...");
+						Console.Read();
+						ok = true;
+					}
+					if (pen.mot.Contains(let[0]) == false)
+					{
+						coup -= 1;
+						Console.WriteLine("Non ! Coup(s) restant(s): " + coup);
+					}
+					if (coup <= 0)
+					{
+						Console.WriteLine("Perdu ! Le mot était: " + pen.mot);
+						Console.Write("Appuyez sur une touche pour continuer...");
+						Console.Read();
+						ok = true;
+					}
+				}
+				catch (System.IndexOutOfRangeException e)
+				{
+					Console.WriteLine("Erreur !");
+					Console.WriteLine(string.Join("", pen.motca.ToArray()));
+				}
+			}
+		}
+	}
+}
