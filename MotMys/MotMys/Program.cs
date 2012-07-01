2eae3898817985557e61d8c59731d4707ced9ac8
Coco <corentin.chataignon@gmail.com>
2012-07-01 11:51:02 +0200

    commit3

---
+++ MotMys/MotMys/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MotMys
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Jeu jeu = new Jeu();
+			List<string> mots = new List<string> { "cochon", "vache", "taureau" };
+			Console.WriteLine(jeu.ShuffleList(mots)[0]);
+		}
+	}
+}
